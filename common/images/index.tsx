a95bb7c6c64009ab8c3c7fe1d4705af765fd2e4b
Kun Zhu <zkzhu@uwaterloo.ca>
2023-12-23 01:07:28 -0500

    move pics to public folder

--- common/images/index.tsx
+++
@@ -1,7 +0,0 @@
-// /src/images/index.js
-import one from './one.jpg';
-import two from './two.jpg';
-import three from './three.jpg';
-import four from './four.jpg';
-
-export { one, two, three, four };
