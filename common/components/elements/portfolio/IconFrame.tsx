40ca35882bb2be5fd346a01134688cc3326d83cf
Kun Zhu <zkzhu@uwaterloo.ca>
2023-12-25 13:40:46 -0500

    factor out icon frame

---
+++ common/components/elements/portfolio/IconFrame.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import * as myPics from "../../../../public/aboutImages"
+import { generateRandomItem } from "../../../utils/random";
+import { IconWrapper } from "../../views/portfolio/PortfolioStyles";
+
+export default function IconFrame() {
+    const myPicsArray = Object.values(myPics);
+
+    return (
+        <Image
+            src={generateRandomItem(myPicsArray)}
+            alt="Kun"
+            style={IconWrapper}
+            placeholder="blur"
+            quality={75}
+        />
+    )
+}
