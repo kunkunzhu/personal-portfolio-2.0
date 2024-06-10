485512221d5e0e2a87a206b15e3c50f425840e58
Kun Zhu <zkzhu@uwaterloo.ca>
2024-06-10 00:38:09 -0400

    revamp exp section

--- common/components/views/portfolio/MiscPortfolio.tsx
+++ common/components/views/portfolio/MiscPortfolio.tsx
@@ -1,116 +1,127 @@
-import React, {ReactNode, useState} from "react";
-// import Image from "next/image";
-import expData from "@/common/data/portfolio/experience.js"
-// import { generateRandomItem } from "@/common/utils/random";
+import React, { ReactNode, useState } from "react";
+import {
+  extraExpData,
+  workExpData,
+} from "@/common/data/portfolio/experience.js";
 import IconFrame from "@/common/components/elements/portfolio/IconFrame";
 import PortfolioBox from "@/common/components/elements/portfolio/PortfolioBox";
-import { 
-    AllExpWrapper,
-    ExpDescription,
-    ExpPilLWrapper,
-    ExpTitle,
-    ExpYearWrapper,
-    Header, 
-    Arrow,
-    // ClubImageWrapper,
+import {
+  AllExpWrapper,
+  ExpDescription,
+  ExpPilLWrapper,
+  ExpTitle,
+  ExpYearWrapper,
+  Header,
+  PromptWrapper,
+  TogglePill,
+  TogglePillWrapper,
+  Arrow,
 } from "./PortfolioStyles";
 
-
 interface ExperienceInterface {
-    key: string,
-    role: string,
-    org: string,
-    tagline: string,
-    link: string | null,
-    // images: string[],
-    internship: boolean,
+  key: string;
+  role: string;
+  org: string;
+  tagline?: string[];
+  link: string | null;
+  internship: boolean;
 }
 
 interface YearExperiencesInterface {
-    year: string,
-    exps: ExperienceInterface[];
+  year: string;
+  exps: ExperienceInterface[];
 }
 
 interface YearExperiencesDataInterface {
-    [key: string]: YearExperiencesInterface;
+  [key: string]: YearExperiencesInterface;
 }
 
-const ExpPill = ({ exp } : { exp: ExperienceInterface }) => {
-    return (
-        <ExpPilLWrapper>
-            <div className="text">
-                <ExpTitle>
-                    <div className="role">{exp.role}</div>
-                    <span className="dot">•</span>
-                    {exp.link ? 
-                    <a className="org" href={exp.link} target="_blank">{exp.org}</a>
-                    :
-                    <div className="org">{exp.org}</div>
-                    }
-                </ExpTitle>
-                <ExpDescription>
-                    <div className="tagline">{exp.tagline}.</div>
-                </ExpDescription>
-            </div>
-            {/* <div className="images">
-                {renderExpImages(exp.images)}
-            </div> */}
-        </ExpPilLWrapper>
-    )
-}
+const ExpPill = ({ exp }: { exp: ExperienceInterface }) => {
+  return (
+    <ExpPilLWrapper>
+      <div className="text">
+        <ExpTitle>
+          <div className="role">{exp.role}</div>
+          <span className="dot">•</span>
+          {exp.link ? (
+            <a className="org" href={exp.link} target="_blank">
+              {exp.org}
+            </a>
+          ) : (
+            <div className="org">{exp.org}</div>
+          )}
+        </ExpTitle>
+        {exp.tagline && <ExpDescription>{exp.tagline}.</ExpDescription>}
+      </div>
+    </ExpPilLWrapper>
+  );
+};
 
 export default function MiscPortfolio() {
+  const [isGif, setIsGif] = useState<boolean>(false);
+  const [isWork, setIsWork] = useState<boolean>(true);
 
-    const [isGif, setIsGif] = useState(false)
+  const renderExperiences = (yearData: ExperienceInterface[]) => {
+    let expsArray: Array<ReactNode> = [];
+    yearData.map((exp: ExperienceInterface) =>
+      expsArray.push(<ExpPill exp={exp} key={exp.key} />)
+    );
+    return expsArray;
+  };
 
-    const renderExperiences = (yearData : ExperienceInterface[]) => {
-        let expsArray : Array<ReactNode> = []
-        yearData.map((exp: ExperienceInterface) => (
-            expsArray.push(
-                <ExpPill exp={exp} key={exp.key}/>
-            )
-        ))
-        return expsArray
-    }
+  const renderYears = (expData: YearExperiencesDataInterface) => {
+    const expYears = Object.values(expData);
+    let expYearsArray: Array<ReactNode> = [];
 
-    const renderYears = (expData: YearExperiencesDataInterface) => {
-        const expYears = Object.values(expData)
-        let expYearsArray : Array<ReactNode> = []
-        
-        expYears.map((expYear) => {
-            expYearsArray.push(
-                <ExpYearWrapper>
-                    <div className="year">
-                        {expYear.year}
-                    </div>
-                    <div className="exp">
-                        {renderExperiences(expYear.exps)}
-                    </div>
-                </ExpYearWrapper>
-            )
-            })
-        return expYearsArray
-    }
+    expYears.map((expYear) => {
+      expYearsArray.push(
+        <ExpYearWrapper>
+          <div className="year">{expYear.year}</div>
+          <div className="exp">{renderExperiences(expYear.exps)}</div>
+        </ExpYearWrapper>
+      );
+    });
+    return expYearsArray;
+  };
 
-    return (
-        <div>
-            <PortfolioBox gifBackground={isGif}>
-                <Header>
-                    <div className="title">
-                        Kun <span className="icon">{IconFrame()}</span> thrives in environments of
-                        <div className="circle" onMouseEnter={() => setIsGif(true)} onMouseLeave={() => setIsGif(false)}>interdisciplinary</div> collaboration <span className="flora">✦.˖✶</span>
-                    </div>
-                    <div className="prompt">
-                        For work-related experiences, grab a copy of her resume <a href="/resume.pdf" target="_blank">here</a>. <br/>
-                        Alternatively, check out the spaces she’s been exploring & the communities she’s been a part of &ensp;
-                        <Arrow className="flora">↓</Arrow>
-                    </div>
-                    <hr/>
-                </Header>
-                <AllExpWrapper>
-                    {renderYears(expData)}
-                </AllExpWrapper>
-            </PortfolioBox>
-        </div>
-    )
+  return (
+    <div>
+      <PortfolioBox gifBackground={isGif}>
+        <Header>
+          <div className="title">
+            Kun <span className="icon">{IconFrame()}</span> thrives in spaces of
+            interdisciplinary
+            <div
+              className="circle"
+              onMouseEnter={() => setIsGif(true)}
+              onMouseLeave={() => setIsGif(false)}
+            >
+              collaboration
+            </div>{" "}
+            <span className="flora">✦.˖✶</span>
+          </div>
+          <PromptWrapper>
+            <div className="prompt">
+              Scroll to see the spaces she’s been exploring &ensp;
+              <Arrow className="flora">↓</Arrow>
+            </div>
+            <TogglePillWrapper>
+              <TogglePill onClick={() => setIsWork(true)} $current={isWork}>
+                work
+              </TogglePill>
+              <TogglePill onClick={() => setIsWork(false)} $current={!isWork}>
+                not work
+              </TogglePill>
+            </TogglePillWrapper>
+          </PromptWrapper>
+          <hr />
+        </Header>
+        {isWork ? (
+          <AllExpWrapper>{renderYears(workExpData)}</AllExpWrapper>
+        ) : (
+          <AllExpWrapper>{renderYears(extraExpData)}</AllExpWrapper>
+        )}
+      </PortfolioBox>
+    </div>
+  );
 }
--- common/data/portfolio/experience.js
+++ common/data/portfolio/experience.js
@@ -1,133 +1,170 @@
-const expData = {
-    y24: {
-        year: "2024",
-        exps: [
-            {
-                key: "canblab",
-                role: "Undergraduate Research Assistant",
-                org: "Cognition and Natural Behaviour Lab",
-                tagline: "",
-                // images: [],
-                link: "https://uwaterloo.ca/cognition-and-natural-behaviour-laboratory/",
-                internship: false
-            },
-            {
-                key: "muomentorship",
-                role: "Mentor",
-                org: "Math Peer Pair program",
-                tagline: "guiding first year student in navigating their undergrad experience",
-                link: null,
-                // images: [],
-                internship: false
-            },
-            {
-                key: "wimdrp2",
-                role: "Mentee",
-                org: "Women in Mathematics Directed Learning Program",
-                tagline: "experimenting with the story generation potentials of LLM",
-                link: null,
-                // images: [],
-                internship: false
-            },
-        ]
-    },
-    y23: {
-        year: "2023",
-        exps: [
-            // {
-            //     key: "oncai",
-            //     role: "Software Engineer",
-            //     org: "Onc.AI",
-            //     tagline: "",
-            //     link: null,
-            //     internship: true,
-            // },
-            {
-                key: "wimdrp1",
-                role: "Mentee",
-                org: "Women in Mathematics Directed Learning Program",
-                tagline: "exploring transfer learning from a statistical perspective",
-                link: null,
-                // images: [],
-                internship: false
-            },
-            {
-                key: "dsc",
-                role: "VP of Design",
-                org: "UW Data Science Club",
-                tagline: "driving the visual branding for the largest academic club on campus",
-                link: "https://www.uwdatascience.ca/",
-                // images: [],
-                internship: false
-            },
-            {
-                key: "technova3",
-                role: "Mentor, Workshop Lead",
-                org: "TechNova",
-                tagline: "crafting resources for beginners to learn about ux design",
-                link: null,
-                // images: ['technova1.jpg', 'technova2.jpg', 'technova3.png'],
-                internship: false
-            },
-            // {
-            //     key: "rondeivu",
-            //     role: "Product Designer",
-            //     org: "Rondeivu",
-            //     tagline: "",
-            //     link: null,
-            //     internship: true,
-            // },
-            {
-                key: "reboot",
-                role: "Fellow",
-                org: "Reboot Student Fellowship",
-                tagline: "discoursing on the intersection between education and technology",
-                link: "https://joinreboot.org/p/announcing-the-2023-reboot-student",
-                // images: [],
-                internship: false
-            },
-        ]
-    },
-    y22: {
-        year: "2022",
-        exps: [
-            {
-                key: "uwux",
-                role: "DEI Lead",
-                org: "UW UX",
-                tagline: "initiating conversations about accessible and socially responsible design",
-                link: "https://www.instagram.com/uw_ux/",
-                // images: [],
-                internship: false
-            },
-            // {
-            //     key: "perspect",
-            //     role: "Software Developer",
-            //     org: "Perspect Analytics",
-            //     tagline: "",
-            //     link: null,
-            //     internship: true,
-            // },
-            {
-                key: "techplus",
-                role: "Logistics Lead",
-                org: "Tech+",
-                tagline: "fostering a more inclusive community for underrepresented students in tech",
-                link: "https://www.techplusuw.com/",
-                // images: [],
-                internship: false
-            },
-            {
-                key: "technova2",
-                role: "Design Lead",
-                org: "TechNova",
-                tagline: "spearheading creative projects for a global gender empowerment hackathon",
-                link: "https://2022.itstechnova.org/",
-                // images: [],
-                internship: false
-            },
-        ]
-    },
-}
+export const workExpData = {
+  y24: {
+    year: "2024",
+    exps: [
+      {
+        key: "vl",
+        role: "Software Engineer",
+        org: "Recursion",
+        link: "https://www.recursion.com/",
+        internship: true,
+      },
+    ],
+  },
+  y23: {
+    year: "2023",
+    exps: [
+      {
+        key: "oncai",
+        role: "Software Engineer",
+        org: "Onc.AI",
+        link: "https://onc.ai/",
+        internship: true,
+      },
+      {
+        key: "rondeivu",
+        role: "Product Designer",
+        org: "Rondeivu",
+        link: "https://rondeivu.com/",
+        internship: true,
+      },
+    ],
+  },
+  y22: {
+    year: "2022",
+    exps: [
+      {
+        key: "perspect",
+        role: "Software Developer",
+        org: "Perspect Analytics",
+        link: "https://perspectanalytics.com/",
+        internship: true,
+      },
+    ],
+  },
+  y21: {
+    year: "2021",
+    exps: [
+      {
+        key: "eb",
+        role: "Software Developer",
+        org: "eButterfly",
+        link: "https://www.e-butterfly.org/",
+        internship: true,
+      },
+    ],
+  },
+};
 
-export default expData
+export const extraExpData = {
+  y24: {
+    year: "2024",
+    exps: [
+      {
+        key: "canblab",
+        role: "Research Assistant",
+        org: "Cognition and Natural Behaviour Lab",
+        tagline: [
+          "facilitating studies to uncover insights about metacognitive learning",
+        ],
+        link: "https://uwaterloo.ca/cognition-and-natural-behaviour-laboratory/",
+        internship: false,
+      },
+      {
+        key: "muomentorship",
+        role: "Mentor",
+        org: "Math Peer Pair program",
+        tagline: [
+          "guiding first year student in navigating their undergrad experience",
+        ],
+        link: null,
+        internship: false,
+      },
+      {
+        key: "wimdrp2",
+        role: "Mentee",
+        org: "Women in Mathematics Directed Learning Program",
+        tagline: [
+          "experimenting with story generation via large language models",
+        ],
+        link: null,
+        internship: false,
+      },
+    ],
+  },
+  y23: {
+    year: "2023",
+    exps: [
+      {
+        key: "wimdrp1",
+        role: "Mentee",
+        org: "Women in Mathematics Directed Learning Program",
+        tagline: ["exploring transfer learning from a statistical perspective"],
+        link: null,
+        internship: false,
+      },
+      {
+        key: "dsc",
+        role: "VP of Design",
+        org: "UW Data Science Club",
+        tagline: [
+          "driving the visual branding for the largest academic club on campus",
+        ],
+        link: "https://www.uwdatascience.ca/",
+        internship: false,
+      },
+      {
+        key: "technova3",
+        role: "Mentor, Workshop Lead",
+        org: "TechNova",
+        tagline: ["crafting resources for beginners to learn about ux design"],
+        link: null,
+        internship: false,
+      },
+      {
+        key: "reboot",
+        role: "Fellow",
+        org: "Reboot Student Fellowship",
+        tagline: [
+          "discoursing on the intersection between education and technology",
+        ],
+        link: "https://joinreboot.org/p/announcing-the-2023-reboot-student",
+        internship: false,
+      },
+    ],
+  },
+  y22: {
+    year: "2022",
+    exps: [
+      {
+        key: "uwux",
+        role: "DEI Lead",
+        org: "UW UX",
+        tagline: ["initiating conversations about socially responsible design"],
+        link: "https://www.instagram.com/uw_ux/",
+
+        internship: false,
+      },
+      {
+        key: "techplus",
+        role: "Logistics Lead",
+        org: "Tech+",
+        tagline: [
+          "fostering a community for underrepresented students in tech",
+        ],
+        link: "https://www.techplusuw.com/",
+        internship: false,
+      },
+      {
+        key: "technova2",
+        role: "Design Lead",
+        org: "TechNova",
+        tagline: [
+          "defining the creative vision for a gender empowerment hackathon",
+        ],
+        link: "https://2022.itstechnova.org/",
+        internship: false,
+      },
+    ],
+  },
+};
